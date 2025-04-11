6a12b064b8b94f491019df13114c0ef30adc56c5
서윤하 <66425844+bobo9245@users.noreply.github.com>
2025-04-11 13:44:00 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 520 ms, Memory: 252844 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,18 @@
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+N = int(input())
+A = [*map(int, input().split())]
+
+# Longgest Increasing Subsequence
+LIS = [A[0]]
+
+for item in A:
+    if LIS[-1] < item:
+        LIS.append(item)
+    else:
+        idx = bisect_left(LIS, item)
+        LIS[idx] = item
+
+print(len(LIS))
