77b54d40992399decbd68095821d1160545b8f03
서윤하 <66425844+bobo9245@users.noreply.github.com>
2025-10-13 17:49:32 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 376 ms, Memory: 117640 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -1,55 +1,41 @@
 import sys
 
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-class SegTree:
-    def __init__(self, arr):
-        self.n = len(arr)
-        self.arr = arr
-        self.tree = [(0, 0)] * (4 * self.n)
-        self._init(1, 0, self.n - 1)
-
-    def _init(self, node, start, end):
-        if start == end:
-            self.tree[node] = (self.arr[start], self.arr[start])
-            return self.tree[node]
-        
-        mid = (start + end) // 2
-        left_child = self._init(node * 2, start, mid)
-        right_child = self._init(node * 2 + 1, mid + 1, end)
-        
-        min_val = min(left_child[0], right_child[0])
-        max_val = max(left_child[1], right_child[1])
-        
-        self.tree[node] = (min_val, max_val)
-        return self.tree[node]
-
-    def _query(self, node, start, end, left, right):
-        if left > end or right < start:
-            return (sys.maxsize, 0)
-        
-        if left <= start and end <= right:
-            return self.tree[node]
-            
-        mid = (start + end) // 2
-        left_res = self._query(node * 2, start, mid, left, right)
-        right_res = self._query(node * 2 + 1, mid + 1, end, left, right)
-        
-        min_val = min(left_res[0], right_res[0])
-        max_val = max(left_res[1], right_res[1])
-        
-        return (min_val, max_val)
-
-    def query(self, left, right):
-        return self._query(1, 0, self.n - 1, left, right)
-
 n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
-segtree = SegTree(arr)
+min_tree = [0] * (2 * n)
+max_tree = [0] * (2 * n)
+
+for i in range(n):
+    min_tree[n + i] = arr[i]
+    max_tree[n + i] = arr[i]
+
+for i in range(n - 1, 0, -1):
+    min_tree[i] = min(min_tree[i * 2], min_tree[i * 2 + 1])
+    max_tree[i] = max(max_tree[i * 2], max_tree[i * 2 + 1])
+
+def query(left, right):
+    left += n
+    right += n
+    min_val = sys.maxsize
+    max_val = 0
+    
+    while left < right:
+        if left % 2:
+            min_val = min(min_val, min_tree[left])
+            max_val = max(max_val, max_tree[left])
+            left += 1
+        if right % 2:
+            right -= 1
+            min_val = min(min_val, min_tree[right])
+            max_val = max(max_val, max_tree[right])
+        left //= 2
+        right //= 2
+        
+    return min_val, max_val
 
 for _ in range(m):
     a, b = map(int, input().split())
-    min_val, max_val = segtree.query(a - 1, b - 1)
-    print(min_val, max_val)
+    min_res, max_res = query(a - 1, b)
+    print(min_res, max_res)
