91a345c846583d43682e33e18e0eb13623ed8b1f
서윤하 <66425844+bobo9245@users.noreply.github.com>
2026-02-10 22:59:26 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+t = input()
+
+if (t == "M"):
+    print("MatKor")
+elif (t == "W"):
+    print("WiCys")
+elif (t == "C"):
+    print("CyKor")
+elif (t == "A"):
+    print("AlKor")
+elif (t == "$"):
+    print("$clear")
